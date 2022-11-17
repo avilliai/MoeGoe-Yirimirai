7d3597f4abd58275b77ae55648596a14f6e2457a
DEII <1840094972@qq.com>
2022-11-17 09:57:42 +0800

    增加群聊切换模型，代码优化

--- bot.py
+++ bot.py
@@ -9,25 +9,23 @@ import sys
 
 from plugins.RandomStr.RandomStr import random_str
 from plugins.picGet import pic
-from readConfig import readConfig
 from trans import translate
 
 if __name__ == '__main__':
     bot = Mirai(3093724179, adapter=WebSocketAdapter(
         verify_key='1234567890', host='localhost', port=23456
     ))
+    aimFriend = 1840094972
+    aimGroup = 699455559
+    statusPath = 1
+    model = 0
+    lang = '中文'
     @bot.on(FriendMessage)
     async def yuYinMode(event: FriendMessage):
         if str(event.message_chain).startswith('发送'):
             sa = str(event.message_chain)[2:]
             ranpath = random_str()
             out = sys.argv[0][:-20] + 'PythonPlugins\\plugins\\voices\\' + ranpath + '.wav'
-            list1=readConfig()
-            aimFriend=list1[0]
-            aimGroup = list1[1]
-            statusPath = list1[2]
-            model = int(list1[3])
-            lang = list1[4]
 
             if int(statusPath)==0:
                 if lang=='中文':
@@ -50,7 +48,6 @@ if __name__ == '__main__':
     #图片模块
     @bot.on(GroupMessage)
     async def handle_group_message(event: GroupMessage):
-        #if str(event.message_chain) == '/pic':
         if '/pic' in str(event.message_chain):
             picNum=int((str(event.message_chain))[4:])
             if picNum<10 and picNum>-1:
@@ -75,10 +72,8 @@ if __name__ == '__main__':
                 if(os.path.exists(out)):
                     os.remove(out)
                 tex= '[ZH]'+((str(event.message_chain))[3:])+'[ZH]'
-                with open('ModePath.txt', mode='r', encoding='utf-8') as fa:
-                    model = fa.readline()
-                    fa.close()
-                    print('当前模型' + model)
+                global model
+                print('当前模型' + model)
                 voiceGenerate(tex, out,int(model))
                 await bot.send(event, Voice(path=out))
             else:
@@ -98,10 +93,8 @@ if __name__ == '__main__':
                 out = sys.argv[0][:-20]+'PythonPlugins\\plugins\\voices\\' + ranpath + '.wav'
                 tex = '[JA]' + translate((str(event.message_chain))[2:]) + '[JA]'
                 #获取当前模型
-                with open('ModePath.txt', mode='r', encoding='utf-8') as fa:
-                    model = fa.readline()
-                    fa.close()
-                    print('当前模型' + model)
+                global model
+                print('当前模型' + model)
                 voiceGenerate(tex, out,int(model))
                 await bot.send(event,Voice(path=out))
             else:
@@ -134,55 +127,59 @@ if __name__ == '__main__':
     async def on_friend_message(event: FriendMessage):
         if str(event.message_chain).startswith('连接群'):
             sa = str(event.message_chain).split('#')
-            configPath='groupPath.txt'
-            with open(configPath, mode='w', encoding='utf-8') as ff:
-                ff.write(sa[1])
-                await bot.send(event,'已载入配置文件')
-            statusPath = 'status.txt'
-            with open(statusPath, mode='w', encoding='utf-8') as fs:
-                fs.write(str(1))
-                await bot.send(event, '已切换为群聊'+sa[1])
+            global aimGroup
+            aimGroup=int(sa[1])
+            global statusPath
+            statusPath = 1
+            await bot.send(event, '已切换为群聊'+sa[1])
 
     #连接人
     @bot.on(FriendMessage)
     async def on_friend_message(event: FriendMessage):
         if str(event.message_chain).startswith('连接对象'):
             sa = str(event.message_chain).split('#')
-            configPath = 'friendPath.txt'
-            with open(configPath, mode='w', encoding='utf-8') as ff:
-                ff.write(sa[1])
-                await bot.send(event, '已载入配置文件')
-
-            statusPath = 'status.txt'
-            with open(statusPath, mode='w', encoding='utf-8') as fs:
-                fs.write(str(0))
-                await bot.send(event, '已切换为私聊对象'+sa[1])
+            global aimFriend
+            aimFriend=sa[1]
+            global statusPath
+            statusPath=0
+            await bot.send(event, '已切换为私聊对象'+sa[1])
 
     #语言切换
     @bot.on(FriendMessage)
     async def Lanconfig(event: FriendMessage):
         if str(event.message_chain).startswith('切换'):
             sa = str(event.message_chain)[2:]
-            langPath = 'langPath.txt'
-            with open(langPath, mode='w', encoding='utf-8') as ff:
-                if sa=='中文':
-                    ff.write(sa)
-                    await bot.send(event, '已切换，当前使用语言'+sa)
-                elif sa=='日语':
-                    ff.write(sa)
-                    await bot.send(event, '已切换，当前使用语言' + sa)
-                else:
-                    await bot.send(event, '数值不合法，语言选择：中文/日语')
+            global lang
+            if sa=='中文':
+                lang=sa
+                await bot.send(event, '已切换，当前使用语言'+sa)
+            elif sa=='日语':
+                lang=sa
+                await bot.send(event, '已切换，当前使用语言' + sa)
+            else:
+                await bot.send(event, '数值不合法，语言选择：中文/日语')
     #模型切换
     @bot.on(FriendMessage)
     async def on_friend_message(event: FriendMessage):
         if str(event.message_chain).startswith('M'):
             sa = str(event.message_chain).split('#')
-            ModePath = 'ModePath.txt'
             modelList = ['0', '1', '2', '3']
-            with open(ModePath, mode='w', encoding='utf-8') as ff:
+            if sa[1] in modelList:
+                global model
+                model=int(sa[1])
+                await bot.send(event, '已切换，当前使用模型' + sa[1])
+            else:
+                await bot.send(event, '数值不合法，模型范围[0-3]')
+
+        # 模型切换
+        @bot.on(GroupMessage)
+        async def on_friend_message(event: GroupMessage):
+            if str(event.message_chain).startswith('M'):
+                sa = str(event.message_chain).split('#')
+                modelList = ['0', '1', '2', '3']
                 if sa[1] in modelList:
-                    ff.write(sa[1])
+                    global model
+                    model = int(sa[1])
                     await bot.send(event, '已切换，当前使用模型' + sa[1])
                 else:
                     await bot.send(event, '数值不合法，模型范围[0-3]')
